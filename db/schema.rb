e7f3b3a7b8a8ffbed002539df05600654fd52467
Abby Barnes <barnesab19@gmail.com>
2021-03-27 19:54:04 +0000

    beginning work on bookmarking

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_164912) do
+ActiveRecord::Schema.define(version: 2021_03_27_194039) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.date "birthdate"
     t.string "bio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "painting_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
